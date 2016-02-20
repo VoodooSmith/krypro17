d76d4634779c4bf2173033e0e1c6904e3193538b
VoodooSmith <Thomas.Schmiedecker@stud.fh-campuswien.ac.at>
2016-02-20 21:10:32 +0100

    Creating project structure

---
+++ krypro17/Client_Server/Client_Server_Conn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Net;
+using System.Collections;
+using System.Numerics;
+
+namespace Client_Server
+{
+    class Client_Server_Conn
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ krypro17/RSA_Encrypt_Decrypt/RSA_Encrypt_Decrypt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Net;
+using System.Collections;
+using System.Numerics;
+
+namespace RSA
+{
+    class RSA_Encrypt_Decrypt
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I'm fucking alive!");
+            Console.WriteLine("Press a fuckin' key...");
+            Console.ReadKey();
+        }
+    }
+}
+/*
+    - RSA Encrypt / Decrypt / Key Generation
+    - EC Skalare Multiplikation
+    - DSA Signature Generation & Verification
+    - HASH of arbitrary texts with arbitraty functions
+    - X.509v3 Certificates reading, generating, verifying
+    - Client / Server Architecture
+    - HMAC
+*/
