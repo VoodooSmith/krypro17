6b9aa6a4c2c4a1f323c315d9ab4300a64b1d4f31
VoodooSmith <Thomas.Schmiedecker@stud.fh-campuswien.ac.at>
2016-02-20 20:21:41 +0100

    Revert "Refactoring"
    This reverts commit 95fcf939f6c53d4b3118749839dbf8d7ddc68a5e.

--- krypro17/RSA_Encrypt_Decrypt/Program.cs
+++ krypro17/RSA_Encrypt_Decrypt/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.IO;
-using System.Security.Cryptography.X509Certificates;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,14 +6,10 @@ using System.Threading.Tasks;
 
 namespace RSA_Encrypt_Decrypt
 {
-    class RSA
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("I'm fucking alive!");
-            Console.WriteLine("Press any key to fuck off...");
-            Console.ReadKey();
         }
     }
 }
