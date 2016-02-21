12acdbfa1414c251611997ea35dff23e472e3793
VoodooSmith <Thomas.Schmiedecker@stud.fh-campuswien.ac.at>
2016-02-21 14:21:15 +0100

    Bugfix

---
+++ krypro17/D/DSA_Gen_Read_Verify.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA
+{
+    class DSA_Gen_Read_Verify
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
