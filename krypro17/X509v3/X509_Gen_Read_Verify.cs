a59b93bd3fe2adef76ad8aab31e562957f6b8a81
VoodooSmith <Thomas.Schmiedecker@stud.fh-campuswien.ac.at>
2016-02-21 13:41:06 +0100

    Project re-structured

---
+++ krypro17/X509v3/X509_Gen_Read_Verify.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace X509v3
+{
+    class X509_Gen_Read_Verify
+    {
+    }
+}
