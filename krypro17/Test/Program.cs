79a2a3a115afa7bea438677cbdb052eef9e351d8
Schmiedeckert <thomas.schmiedecker@stud.fh-campuswien.ac.at>
2016-02-21 13:08:05 +0100

    Test auf MAC

---
+++ krypro17/Test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Test
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			Console.WriteLine ("Huhu");
+		}
+	}
+}
