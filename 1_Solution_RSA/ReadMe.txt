57a27ebdfa9b723ee04f6ce33df1a7d44288c1bc
VoodooSmith <Thomas.Schmiedecker@stud.fh-campuswien.ac.at>
2016-03-08 00:12:27 +0100

    Hashing finished ReadMe added to RSA

---
+++ 1_Solution_RSA/ReadMe.txt
@@ -0,0 +1,47 @@
+###############
+#	      #
+#  RSA Setup  #
+# 	      #
+###############
+
+
+Solution Parts
+******************************
+1)	RSA_Encrypt_Decrypt.cs
+2)	ExtendedEuclidianAlog.cs
+3)	MRT.cs
+
+
+Build Information
+******************************
+Target-Framework 4.5 (compatible with Apple)
+
+If References are missing, add the following:
+	1)	Extended_Euclidian
+	2)	MillerRabinTest
+
+How to add references with Visual Studio:
+	1)	Right click on project name
+	2)	Add Reference...
+	3)	Navigate to "Projects"
+	4)	Tick Extended_Euclid and MillerRabinTest
+
+Import Euclid and MillerRabin classes to RSA with the "using"-statement
+
+
+Program structure
+******************************
+x) Program starts and generates private and public keys
+x) User will be asked for input
+x) Encryption and Decryption values will be displayed
+x) Decrypted string will be displayed to control the algorithm
+
+
+IMPORTANT
+******************************
+If OutOfRangeException occurs, terminate program and 
+start program again. In this case the prime numbers weren't coprime.
+	
+
+
+
---
+++ 2_Solution_Hash/Hash/Text_Hashing.cs
@@ -0,0 +1,83 @@
+﻿/*
+ 
+  HASH of arbitrary texts with arbitraty functions
+  Cryptographic Protocolls ITS17
+  Author: Thomas Schmiedecker
+  23. 02. 2016
+  
+*/
+
+using System;
+
+namespace Hash
+{
+    class Text_Hashing
+    {
+        static void Main(string[] args)
+        {
+            System.Security.Cryptography.HashAlgorithm secHash = null;
+            int hashAlgo = 0;
+
+            /* Selection of hash algorithm */
+            Console.WriteLine("Please select a hash algorithm\n");
+            Console.WriteLine("Press 1 for MD5");
+            Console.WriteLine("Press 2 for SHA256");
+            Console.WriteLine("Press 3 for SHA384");
+            Console.WriteLine("Press 4 for SHA512\n");
+            Console.WriteLine("Confirm with ENTER");
+            Console.Write("Choice: ");
+            hashAlgo = Convert.ToInt32(Console.ReadLine());
+
+            switch (hashAlgo)
+            {
+                case 1:
+                    Console.WriteLine("Hash algorithm MD5 selected\n");
+                    secHash = new System.Security.Cryptography.MD5CryptoServiceProvider();
+                    break;
+                case 2:
+                    Console.WriteLine("Hash algorithm SHA256 selected\n");
+                    secHash = new System.Security.Cryptography.SHA256CryptoServiceProvider();
+                    break;
+                case 3:
+                    Console.WriteLine("Hash algorithm SHA384 selected\n");
+                    secHash = new System.Security.Cryptography.SHA384CryptoServiceProvider();
+                    break;
+                case 4:
+                    Console.WriteLine("Hash algorithm SHA512 selected\n");
+                    secHash = new System.Security.Cryptography.SHA512CryptoServiceProvider();
+                    break;
+                default:
+                    Console.WriteLine("Default case");
+                    break;
+            }
+
+            /* User input */
+            Console.Write("Please enter your text here: ");
+            string input = Console.ReadLine();
+            byte[] asciitext = System.Text.Encoding.ASCII.GetBytes(input);
+
+            /* Hashing of user input */
+            byte[] textHash = secHash.ComputeHash(asciitext);
+
+            /* Hashed output as byte array*/
+            Console.Write("Hashed text: ");
+            Display(textHash);
+           
+            Console.WriteLine("End of code");
+            Console.WriteLine("Press any key to end");
+            Console.ReadKey();
+        }
+
+        /* Display bytearray */
+        static void Display(byte[] array)
+        {
+            /* Loop through array and display bytes */
+            foreach (byte value in array)
+            {
+                Console.Write(value);
+                Console.Write(' ');
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
