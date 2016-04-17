0fe45553708432930d08784315e1bcfda1875f7b
VoodooSmith <Thomas.Schmiedecker@stud.fh-campuswien.ac.at>
2016-04-17 08:53:35 +0200

    Unnessecary code removed

--- CurrencyConverterService/CurrencyConverter/CurrencyConverterService.svc.cs
+++ CurrencyConverterService/CurrencyConverter/CurrencyConverterService.svc.cs
@@ -33,60 +33,22 @@ namespace CurrencyConverterService
 
         }
 
-        public double GetCurrency(string curr)
-        {
-            double value = 0.0;
+        //public double GetCurrency(string curr)
+        //{
+        //    double value = 0.0;
 
-            XmlDocument doc = new XmlDocument();
-            doc.Load("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
-            XmlElement root = doc.DocumentElement;
-            XmlNodeList nodes = root.SelectNodes("gesmes:Envelope");
-            Console.WriteLine(doc.DocumentElement);
+        //    XmlDocument doc = new XmlDocument();
+        //    doc.Load("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
+        //    XmlElement root = doc.DocumentElement;
+        //    XmlNodeList nodes = root.SelectNodes("gesmes:Envelope");
+        //    Console.WriteLine(doc.DocumentElement);
 
-            foreach (XmlNode node in nodes)
-            {
-                Console.WriteLine(node);
-            }
-            //XmlNamespaceManager nsmgr = new XmlNamespaceManager(doc.NameTable);
-            //nsmgr.AddNamespace("money", "schema");
-            //XmlNodeList nodes = root.SelectNodes("Envelope/Cube");
-
-            //foreach (XmlNode node in root)
-            //{
-            //    string currency = node["temp_f"].InnerText;
-            //    string rate = node["temp_c"].InnerText;
-
-            //    Console.WriteLine("Currency: {0}\nRate: {1}\n", currency, rate);
-
-            //}
-            return value;
+        //    foreach (XmlNode node in nodes)
+        //    {
+        //        Console.WriteLine(node);
+        //    }
+        //    //    Console.WriteLine("Currency: {0}\nRate: {1}\n", currency, rate);
+        //    return value;
         }
-
-    //public CompositeType GetCurrGet(CompositeType exec)
-    //{
-    //    if(exec ==null)
-    //    {
-    //        throw new ArgumentNullException("composite");
-    //    }
-    //    if(exec.BoolValue)
-    //    {
-    //        exec.StringValue += "Suffix";
-    //    }
-    //    return exec;
-    //}
-
-
-    //public CompositeType GetDataUsingDataContract(CompositeType composite)
-    //{
-    //    if (composite == null)
-    //    {
-    //        throw new ArgumentNullException("composite");
-    //    }
-    //    if (composite.BoolValue)
-    //    {
-    //        composite.StringValue += "Suffix";
-    //    }
-    //    return composite;
-    //}
-}
+    }
 }
